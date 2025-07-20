0ba335d1156cb43476b921adbd280a7f6a6fcca8
AngHY07 <jaydenanghy@gmail.com>
2025-07-20 14:11:05 +0800

    s

--- Mini Task 02/Decisions.py
+++ Mini Task 02/Decisions.py
@@ -7,10 +7,10 @@ temperature = float(input("Enter current temperature: "))
 random_value = random.randint(0,4)
 
 if temperature < 0 :
-    out_or_in = "go to the moon and escape the cold"
+    out_or_in = "go to the moon to escape the cold"
 elif temperature > 30: 
     out_or_in = "go to the beach to enjoy the sun"
 else : 
-    out_or_in = "stay home and relax with cup of coffee"
+    out_or_in = "stay home and relax with a cup of coffee"
 
-print (f"The temperature for today is {temperature} degree celcius so i will {out_or_in}. I am feeling very {Bot_Mood[random_value]} now.")
+print (f"The temperature for today is {temperature} degree celsius so i will {out_or_in}. I am feeling very {Bot_Mood[random_value]} now.")
